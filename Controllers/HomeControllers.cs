873c8d6f200af5913b69421e6cc3515ab10466df
Chua Wei Zhe <chuazhe@gmail.com>
2019-04-05 16:42:18 +0800

    Learning

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name; // string can set to null so need ?
+                decimal? price = p?.Price; //decimal cannot set to null so put ?
+                string relatedName = p?.Related?.Name;
+
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",
+                name, price, relatedName));
+            }
+            return View(results);
+        }
+    }
+}
