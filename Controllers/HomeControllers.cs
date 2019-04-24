01e64461a16dab847d4511c83badb852bc5bb993
Chua Wei Zhe <chuazhe@gmail.com>
2019-04-24 14:17:29 +0800

    Learning

--- Controllers/HomeControllers.cs
+++ Controllers/HomeControllers.cs
@@ -11,12 +11,20 @@ namespace LanguageFeatures.Controllers
             List<string> results = new List<string>();
             foreach (Product p in Product.GetProducts())
             {
-                string name = p?.Name; // string can set to null so need ?
-                decimal? price = p?.Price; //decimal cannot set to null so put ?
-                string relatedName = p?.Related?.Name;
+                // ?? mean if null, change to this value
+                string name = p?.Name ?? "<No Name>"; // string can set to null so don need ?, product is a object so also nid ?
+                decimal? price = p?.Price ?? 0; //decimal cannot set to null so put ?
+                string relatedName = p?.Related?.Name ?? "<None>"; // Related is a object, so nid ?
 
+                /*
                 results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",
-                name, price, relatedName));
+                name, price, relatedName));*/
+
+                // String Interpolation
+                /*String interpolation supports all the format specifiers that are available with the string.Format
+                  method. The format specifiers are included as part of the hole, so $"Price: {price:C2}" would format the
+                  price value as a currency value with two decimal digits.*/
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
             }
             return View(results);
         }
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,15 +2,21 @@
 {
     public class Product
     {
+        //Automatically Implemented Properties
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
 
+        public bool InStock { get; } = true; // read only
+
         public static Product[] GetProducts()
         {
+            //object initializer
             Product kayak = new Product
             {
                 Name = "Kayak",
+                Category = "Water Craft",
                 Price = 275M
             };
             Product lifejacket = new Product
